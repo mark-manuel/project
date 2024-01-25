f5f70bad0aad006a39764b4cce0ae527851870e0
mark-manuel <155622877+mark-manuel@users.noreply.github.com>
2024-01-25 15:27:17 +0530

    Update dob.java

--- dob.java
+++ dob.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    String dob = "01/01/2003";
+    String dob = "01/01/2003" & "02/08/2004";
     System.out.println("My date of birth is " + dob);
   }
 }
